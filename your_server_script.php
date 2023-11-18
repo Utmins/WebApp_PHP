ce9c44794f5d11aa684a48d876e009fffdf7d26f
Utmins <120234987+Utmins@users.noreply.github.com>
2023-11-18 12:39:21 -0800

    Update your_server_script.php

--- your_server_script.php
+++ your_server_script.php
@@ -22,15 +22,15 @@ if(isset($_GET['username'])) {
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "First Name: " . $row["first_name"];
-            echo "Last Name: " . $row["last_name"];
-            echo "Age: " . $row["age"];
-            echo "Address: " . $row["address"];
-            echo "City: " . $row["city"];
-            echo "State: " . $row["state"];
-            echo "Country: " . $row["country"];
-            echo "Zip Code: " . $row["zip_code"];
-            echo "SIN: " . $row["sin"];
+            echo "First Name: " . $row["first_name"] . "<br>";
+            echo "Last Name: " . $row["last_name"] . "<br>";
+            echo "Age: " . $row["age"] . "<br>";
+            echo "Address: " . $row["address"] . "<br>";
+            echo "City: " . $row["city"] . "<br>";
+            echo "State: " . $row["state"] . "<br>";
+            echo "Country: " . $row["country"] . "<br>";
+            echo "Zip Code: " . $row["zip_code"] . "<br>";
+            echo "SIN: " . $row["sin"] . "<br>";
         }
     } else {
         echo "User not found";
